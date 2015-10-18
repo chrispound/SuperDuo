a7d35b7f059b02db8e9e48ebbe6bb4bfda1f0856
Chris Pound <c2.pound@gmail.com>
2015-10-18 16:25:43 -0500

    color scheme changes added basic widget requirements

---
+++ footballscores/app/src/main/java/barqsoft/footballscores/widget/FootballScoresWidgetProvider.java
@@ -0,0 +1,26 @@
+package barqsoft.footballscores.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+public class FootballScoresWidgetProvider  extends AppWidgetProvider{
+
+    public FootballScoresWidgetProvider()
+    {
+        super();
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+    {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
